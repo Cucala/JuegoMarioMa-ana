4a5381821587a49e2bd9ed2b04ba9f399dd053a5
Jaume Lloret Enríquez <elcucala@gmail.com>
2021-05-24 10:34:42 +0200

    Primera subida

---
+++ Mario/core/src/com/jlloret/mario/screens/FirstLevel.java
@@ -0,0 +1,58 @@
+package com.jlloret.mario.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.jlloret.mario.Mario;
+
+public class FirstLevel implements Screen {
+    private Mario game;
+    private TextureAtlas atlas;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+
+
+
+    public FirstLevel(Mario game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 1);
+        this.game.beginDraw();
+        this.game.endDraw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
